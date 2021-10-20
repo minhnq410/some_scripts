74bedb3bb823a040911782af9388e9b23e6e9ffd
minhnq410 <43316258+minhnq410@users.noreply.github.com>
2021-10-20 11:33:20 +0700

    Add files via upload

--- manager_services_script.sh
+++ manager_services_script.sh
@@ -3,12 +3,12 @@
 wget https://github.com/prometheus/snmp_exporter/releases/download/v0.20.0/snmp_exporter-0.20.0.linux-amd64.tar.gz
 tar zxvf snmp_exporter-0.20.0.linux-amd64.tar.gz
 cp ./snmp_exporter-0.20.0.linux-amd64/snmp_exporter /opt/
-cp ./snmp_exporter.service /etc/systemd/system/
+cp ./config/snmp_exporter.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable --now snmp_exporter.service
 rm -rm snmp_exporter-0.20.0*
 # expose docker daemon metrics
-cp ./daemon.json /etc/docker/
+cp ./config/daemon.json /etc/docker/
 systemctl restart docker.service
 firewall-cmd --permanent --add-port=9323/tcp --zone=public
 firewall-cmd --reload
