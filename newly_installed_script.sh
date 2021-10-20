b5ff46e8629f151cea5f49373834333a6a4a3917
minhnq410 <43316258+minhnq410@users.noreply.github.com>
2021-10-20 11:22:26 +0700

    add scripts for manager

--- newly_installed_script.sh
+++ newly_installed_script.sh
@@ -9,7 +9,7 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install -y docker-ce docker-ce-cli containerd.io
 systemctl enable --now docker.socket docker.service
 # Add rules to firewalld
-firewall-cmd --permanent --add-port=2373/tcp --add-port=9090/tcp --add-port=9100/tcp --add-port=161/udp --add-port=5000/tcp --add-port=8080/tcp --zone=public
+firewall-cmd --permanent --add-port=2377/tcp --add-port=9090/tcp --add-port=9100/tcp --add-port=161/udp --add-port=5000/tcp --add-port=8080/tcp --zone=public
 firewall-cmd --reload
 # Installing Node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
